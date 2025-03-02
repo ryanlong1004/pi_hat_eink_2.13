5916e9d88001d4b6d7e4b821d2a7ee700e12ecf4
Ryan Long <rlong@wasabi.com>
2025-03-02 18:39:33 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import logging
+from epd_display import EPDDisplay
+from waveshare_epd import epd2in13b_V4
+
+logging.basicConfig(level=logging.DEBUG)
+
+def main():
+    try:
+        display = EPDDisplay()
+        display.init_and_clear()
+        display.draw_horizontal_image()
+        display.draw_vertical_image()
+        display.display_bmp_files()
+        display.clear_and_sleep()
+    except IOError as e:
+        logging.info(e)
+    except KeyboardInterrupt:
+        logging.info("ctrl + c:")
+        epd2in13b_V4.epdconfig.module_exit(cleanup=True)
+        exit()
+
+listif __name__ == "__main__":
+    main()
