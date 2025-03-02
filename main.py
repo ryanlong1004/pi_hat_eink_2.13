f387d6c0dca1c85f4ed458783c884a5dfc63ff51
Ryan Long <rlong@wasabi.com>
2025-03-02 18:46:58 -0500

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import sys
 import os
 import logging
-from waveshare_epd import epd2in13b_V4
+from lib.waveshare_epd import epd2in13b_V4
 import time
 from PIL import Image, ImageDraw, ImageFont
 import traceback
