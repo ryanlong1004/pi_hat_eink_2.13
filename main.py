3d1ce1ed4b5f865c7f0afd8fab2606d7d472dd40
Ryan Long <rlong@wasabi.com>
2025-03-02 18:55:16 -0500

    Add images

--- main.py
+++ main.py
@@ -3,12 +3,8 @@
 import sys
 import os
 
-picdir = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "pic"
-)
-libdir = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "lib"
-)
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "pic")
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lib")
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
