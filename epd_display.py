3bba8e6ce5af03d8c65332de50a3152de3eb2ba3
Ryan Long <rlong@wasabi.com>
2025-03-03 20:25:40 -0500

    tests

--- epd_display.py
+++ epd_display.py
@@ -39,6 +39,7 @@ class EPDDisplay:
             logging.info("2.Drawing on the Vertical image...")
             image_size = (self.epd.width, self.epd.height)
         black_image = Image.new("1", image_size, 255)
+        ry_image = Image.new("1", image_size, 255)
         drawblack = ImageDraw.Draw(black_image)
         drawblack.text((10, 0), "hello world", font=FONT_20, fill=0)
         self.epd.display(self.epd.getbuffer(black_image), self.epd.getbuffer(ry_image))
